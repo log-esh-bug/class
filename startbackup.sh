75b5e72ef09bc8ee2e2fe307670e428d527624b6
log-esh-bug <logeshwaranoffl@gmail.com>
2025-03-04 17:27:10 +0530

    modified:   base 	modified:   logfile 	new file:   sl_lock.sh 	modified:   startbackup.sh

--- startbackup.sh
+++ startbackup.sh
@@ -1,23 +1,77 @@
 #!/bin/bash
 parent_dir=/home/logesh-pt7689/script/class/
+db=${parent_dir}base
+markdb=${parent_dir}Marksbase
+topbase=${parent_dir}toppers
+
 backup_dir=${parent_dir}backup_dir
+
 log_script=${parent_dir}dolog.sh
 
 backup_sleep_time=5
+backup_threshold=5
+
+fetch_lock(){
+	while [ -e ${1}.lock ];
+	do
+		# echo "waiting!"
+		sleep 1		
+	done
+	touch ${1}.lock
+}
+
+drop_lock(){
+	if [ -e ${1}.lock ];then
+		rm ${1}.lock
+	fi
+}
+
+cleanup(){
+	drop_lock $db
+	drop_lock $markdb
+	drop_lock $topbase
+}
+trap cleanup EXIT
+
+start_backup_helper(){
+    fetch_lock ${db}
+    fetch_lock ${markdb}
+    fetch_lock ${topbase}
+
+    current_dir=$(pwd)
+    cd $parent_dir
+    tar --create --file ${backup_dir}/base_$(date +%Y%m%d%H%M%S).tar.gz $(basename $db) $(basename $markdb) $(basename $topbase)
+    cd $current_dir
+
+    drop_lock ${db}
+    drop_lock ${markdb}
+    drop_lock ${topbase}
+    $log_script "Done backup"
+}
 
 if [ -n "$1" ]; then
     $log_script "$(basename $0) says Backup sleep time is set to $1"
     backup_sleep_time=$1
 fi
 
-start_backup_helper(){
-    $log_script "Starting backup"
-    
-    $log_script "Backup completed"
-}
+if [ ! -d $backup_dir ];then
+    if [[ $(mkdir $backup_dir)==0 ]];then
+        $log_script "No backup directory found.Created one at $backup_dir"
+    else
+        $log_script "Unable to create backup directory at $backup_dir"
+    fi
+fi
 
 while((1))
 do
+    backups_found=$(ls -l $backup_dir | wc -l)
+    if(($backups_found > $backup_threshold));then
+
+        $log_script "More than $backup_threshold backups found in $backup_dir.Deleting oldest backup"
+
+        oldest_backup=$(ls -t $backup_dir | tail -1)
+        rm $backup_dir/$oldest_backup
+    fi
     start_backup_helper
     sleep $backup_sleep_time
 done
